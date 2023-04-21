ea5eba99f80185d1bc8c510fb24870a7386d3543
Raycris Maldonado <raycris.maldonado@utreee.com>
2023-04-20 21:25:20 -0400

    search component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-
-import './App.css'
+import Searcher from "./components/Searcher";
+import "./App.css";
+import 'antd/dist/reset.css';
+import {Col} from "antd"
 
 function App() {
-
   return (
-    <div>POkedux redux</div>
-  )
+    <div className="container">
+      <h1>Pokedux Redux</h1>
+    <Col span={8} offset={8}>
+      <Searcher />
+    </Col>
+    </div>
+  );
 }
 
-export default App
+export default App;
