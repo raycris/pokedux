f9e8563668fca2853c1e1d34afbcebdf72016096
Raycris Maldonado <raycris.maldonado@utreee.com>
2023-05-01 17:16:59 -0400

    done

---
+++ src/middlewares/index.js
@@ -0,0 +1,13 @@
+export const logger = (store) => (next) => (action) => {
+  console.log(action);
+  next(action);
+};
+
+export const featuring = (store) => (next) => (actionInfo) => {
+  const featured = [{ name: "Messi Goat" }, ...actionInfo.action.payload];
+  const updatedActionInfo = {
+    ...actionInfo,
+    action: { ...actionInfo.action, payload: featured },
+  };
+  next(updatedActionInfo);
+};
